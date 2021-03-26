c36b0e251e7043e3d051b39074a4f6d3895dab5c
Karel de Bruin <kjfdebruin@gmail.com>
2021-03-26 09:17:56 +0200

    Minor homescreen changes not committed last night

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <nav class="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6" aria-label="Global">
     <div class="flex flex-col md:flex-row items-center flex-1">
-      <div class="flex items-center justify-between w-full md:w-auto">
+      <div class="flex items-center justify-center sm:justify-start w-full md:w-auto">
         <a class="flex items-center" href="/">
           <img class="h-24 w-auto mr-6" src="./assets/logo.svg" alt="">
-          <span class="text-2xl font-family-pixelated">
+          <span class="text-2xl font-family-pixelated flex flex-col sm:flex-row">
               <span class="text-blue-500">Framed</span><span class="ml-2">Punks</span>
           </span>
         </a>
